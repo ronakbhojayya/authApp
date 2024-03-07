115715bc29f089f37db69cbbdd3285265b832d7c
ronak <ronakbhojayya@gmail.com>
2024-03-07 15:23:02 +0530

    update added

--- public/script.js
+++ public/script.js
@@ -1,3 +1,15 @@
 setTimeout(function () {
   document.querySelector(".msgBox").style.display = "none";
 }, 5000);
+
+function loadSignUp() {
+  document.querySelector(
+    "#signUp"
+  ).innerHTML = `<i class="fa-solid fa-spinner fa-spin"></i>`;
+}
+
+function loadSignIn() {
+  document.querySelector(
+    "#signIn"
+  ).innerHTML = `<i class="fa-solid fa-spinner fa-spin"></i>`;
+}
